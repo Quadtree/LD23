3a13b45a3c7cebb0161404b33b6b7ad06fe45160
Quadtree <quadtree@gmail.com>
2017-08-01 02:14:38 -0700

    Updated

--- core/src/com/ironalloygames/spaceimperator/core/Explosion.java
+++ core/src/com/ironalloygames/spaceimperator/core/Explosion.java
@@ -1,45 +1,41 @@
 package com.ironalloygames.spaceimperator.core;
 
+import com.badlogic.gdx.math.Vector2;
 
-
-
-
-
-
-public class Explosion extends Actor{
-	Vec2 pos;
-	float size;
-	
+public class Explosion extends Actor {
 	static Image graphic;
-	
-	public Explosion(Vec2 pos, float size)
-	{
+	Vector2 pos;
+
+	float size;
+
+	public Explosion(Vector2 pos, float size) {
 		this.pos = new Vec2(pos);
 		this.size = size;
 	}
 
 	@Override
-	public void update() {
-		size *= 0.8f;
-		super.update();
+	public boolean keep() {
+		return size > 0.01f;
 	}
 
 	@Override
 	public void render(Surface target) {
 		target.save();
 		target.translate(pos.x, pos.y);
-		target.rotate(SpaceImperatorGame.s.rand.nextFloat() * (float)Math.PI * 2);
-		
-		if(graphic == null) graphic = PlayN.assets().getImage("images/shot_hit.png");
-		
+		target.rotate(SpaceImperatorGame.s.rand.nextFloat() * (float) Math.PI * 2);
+
+		if (graphic == null)
+			graphic = PlayN.assets().getImage("images/shot_hit.png");
+
 		target.drawImage(graphic, -size / 2, -size / 2, size, size);
-		
+
 		target.restore();
 		super.render(target);
 	}
 
 	@Override
-	public boolean keep() {
-		return size > 0.01f;
+	public void update() {
+		size *= 0.8f;
+		super.update();
 	}
 }
